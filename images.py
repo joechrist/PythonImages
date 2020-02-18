f7d993283dab9af81839d1cc6052ce838f140199
Joe <christowel@live.fr>
2020-02-17 22:32:58 -0500

    files modified

--- images.py
+++ images.py
@@ -8,8 +8,11 @@ img_squirtle = Image.open('./pokedex/squirtle.jpg')
 filtered_img = img_pika.convert('L')
 rotate_pika = img_pika.rotate(180)
 resize_squirtle = img_squirtle.resize((300, 300))
+pika_box = (100, 100, 400, 400)
+region = filtered_img.crop(pika_box)
 
 # save new files output to see results
 filtered_img.save('pikconvert.png', 'png')
 rotate_pika.save('rotatepika.png', 'png')
 resize_squirtle.save('squirtleresize.png', 'png')
+region.save('charmanderBox.png', 'png')
