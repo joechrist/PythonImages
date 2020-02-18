0c09a9316528cd29585c442870343e83b56baea2
Joe <christowel@live.fr>
2020-02-17 22:47:24 -0500

    delete file and new methods added

--- images.py
+++ images.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageFilter
 # Open file we want to work with
 img_pika = Image.open('./pokedex/pikachu.jpg')
 img_squirtle = Image.open('./pokedex/squirtle.jpg')
+img_astro = Image.open('./astro.jpg')
 
 # Do some actions -- apply some methods
 filtered_img = img_pika.convert('L')
@@ -10,9 +11,11 @@ rotate_pika = img_pika.rotate(180)
 resize_squirtle = img_squirtle.resize((300, 300))
 pika_box = (100, 100, 400, 400)
 region = filtered_img.crop(pika_box)
+img_astro.thumbnail((400, 381))
 
 # save new files output to see results
 filtered_img.save('pikconvert.png', 'png')
 rotate_pika.save('rotatepika.png', 'png')
 resize_squirtle.save('squirtleresize.png', 'png')
 region.save('charmanderBox.png', 'png')
+img_astro.save('astroresize.png', 'png')
