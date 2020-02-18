1ad9d1b431b3c71a3b2bfa160761ed8d2bd32211
Joe <christowel@live.fr>
2020-02-17 23:06:06 -0500

    JPG to PNG convertion

---
+++ convertJPGtoPNG.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from PIL import Image
+
+# Grab first and second argument
+
+#  Check is new/ folder exists, if not create it
+
+# loop to pokedex,
+# Convert images to png,
+# Save into the new folder.
