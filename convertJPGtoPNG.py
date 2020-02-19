1d7bc2acff8c5c06a4ee6ab33e319ad43f460e06
Joe <christowel@live.fr>
2020-02-18 20:50:16 -0500

    convert jpg files to png file

--- convertJPGtoPNG.py
+++ convertJPGtoPNG.py
@@ -3,9 +3,17 @@ import os
 from PIL import Image
 
 # Grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
 
-#  Check is new/ folder exists, if not create it
+#  Check is new/ folder exists, if not create it (make directory)
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 # loop to pokedex,
-# Convert images to png,
+# Convert images to png, /Use splitext() method of os module
 # Save into the new folder.
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_file_ext = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_file_ext}.png', 'png')
